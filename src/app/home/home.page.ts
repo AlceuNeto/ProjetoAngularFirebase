cdc2192ff2c2851a7ff1c1e9c8c1cab078594e00
Alceu Mendes <54826468+AlceuNeto@users.noreply.github.com>
2024-08-09 22:53:59 +0000

    090824 curriculo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,61 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  constructor(){ }
-}
+  pessoa = {
+    foto: `https://i.pinimg.com/474x/ce/f2/ae/cef2aeabb3aff19605a2a952b276ac81.jpg`,
+     nome:`Alceu Mendes de Brito Neto`,
+     objetivo: `Músico e Programador`,
+     contato:{
+      Email:'alceupiano@gmail.com',
+      Telefone:'15-4002-8922',
+      github:"github.com/piano",
+      linkedin:'Linkedin.com/pianoalceu',
+      
+      
+     },
+     softskills: [
+      'Comunicação','Proatividade','Trabalho em Equipe', 'Inovação','rei da putaria'
+  
+    ],
+    formacao:[
+      {
+        ano_inicio: '2023',
+        ano_fim: '2025',
+        instituicao:"ETEC Sales Gomes",
+        curso: 'Desenvolvimento de Sistemas'
+      },
+      {
+        ano_inicio: '2025',
+        ano_fim: '2028',
+        instituicao:"Faculdade de Tecnologia de Tatuí - FATEC",
+        curso: 'Ánalise e Desenvolvimento de Sistemas'
+      }
+      
+
+    ],
+    projetos:[
+      {
+        ano:2023,
+        instituicao:'ETEC SALES GOMES',
+        epa:'Projeto ETEC Portas abertas',
+        sobre: 'abualbualbluablabluebluealubealubaelubea'
+      },
+      {
+        ano:2024,
+        instituicao:'ETEC SALES GOMES',
+        epa:'Projeto ETEC Portas abertas',
+        sobre: 'abualbualbluablabluebluealubealubaelubea'
+      }
+    ]
+     
+    
+
+  }
+  
+
+   
+  };
+
+
+  
+
