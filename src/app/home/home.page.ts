88d40ada17ac72ee8a989247017191001faf020d
Alceu Mendes <54826468+AlceuNeto@users.noreply.github.com>
2024-11-08 22:22:29 +0000

    limpoo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,35 +10,5 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  aluno:any = {
-    nome: null,
-    email: null
-
-  }
-  alunos: any = [
-
-  ]
-
-
-  constructor(
-    public crudService: CrudService
-    
-
-  ){ 
-    this.getAlunos();
-  }
-  cadastrar(){
-    this.crudService.insert(this.aluno,'aluno');
-    this.getAlunos();
-    
-
-    }
-    getAlunos(){
-      this.crudService.fetchAll('aluno')
-      .then(response => {
-        console.log(response);
-        this.alunos = response;
-      })
-   
-  }
+ 
 }
